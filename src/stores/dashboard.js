3fa2ce640f3c53db398050676ca97ffcaf7c92f9
Jerold Arranzado <fleshp1234@gmail.com>
2025-08-10 12:01:05 +0800

    Update dashboard.js

--- src/stores/dashboard.js
+++ src/stores/dashboard.js
@@ -236,7 +236,7 @@ export const useDashboardStore = defineStore('dashboard', () => {
 
   function setupWebSocket() {
     try {
-        websocket.value = new WebSocket('ws://127.0.0.1:8000/ws/status')
+        websocket.value = new WebSocket('wss://discord-adminbot.onrender.com//ws/status')
         websocket.value.onopen = () => {
         console.log('WebSocket connected')
         isConnected    .value = true
